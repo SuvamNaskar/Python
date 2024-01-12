6fc04ef9954e9a76be2d8acbf99f52a680c383b1
= <=>
2024-01-12 10:07:32 +0530

    Floor, Ceil and Rint functions in Python
    Basic operations of `numpy` package

---
+++ sol8.py
@@ -0,0 +1,11 @@
+    # TODO: Floor, Ceil and Rint functions in Python
+
+import numpy
+
+numpy.set_printoptions(sign=' ')
+
+a = numpy.array(input().split(),float)
+
+print(numpy.floor(a))
+print(numpy.ceil(a))
+print(numpy.rint(a))
